765a5af564c438267f2773f3d4896a05a38d0976
DylanHalbig <dylanh030@gmail.com>
2023-10-18 21:50:49 -0400

    added sign out functionality

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,44 +1,59 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 function Home() {
     let navigate = useNavigate();
-    const [getUsername, setUsername] = useState("")
-    
+    const [getUsername, setUsername] = useState("");
+
     useEffect(() => {
         if (localStorage.token) {
-        fetch('http://localhost:8080/user', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': localStorage.token
-            }
-        }).then(response => {
-            if (response.status !== 500) {
-                // console.log(response)
-                response.json().then(res => {
-                    console.log(res)
-                    setUsername(res.username)
-                }).catch(e => {
-                    console.log(e)
-                })
-            } else {
-                console.log("invalid token")
-            }
-        })
+            fetch('http://localhost:8080/user', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': localStorage.token
+                }
+            }).then(response => {
+                if (response.status !== 500) {
+                    response.json().then(res => {
+                        setUsername(res.username);
+                    }).catch(e => {
+                        console.log(e);
+                    });
+                } else {
+                    console.log("invalid token");
+                    navigate("/login");
+                }
+            });
         } else {
-            navigate("/login")
+            navigate("/login");
         }
     }, [navigate]);
 
+    const handleLogout = () => {
+        // Clear the token from localStorage
+        localStorage.removeItem("token");
+
+        // Navigate to the login page after logging out
+        navigate("/login");
+    };
 
     return (
         <div>
             <h1>Home Page</h1>
-            {(localStorage.token) ? 
-            "welcome " + getUsername : 
-            <Link to="/login" className="btn btn-primary">Login</Link>
-            }
+            {(localStorage.token) ? (
+                <div>
+                    <p>Welcome {getUsername}</p>
+                    <button onClick={handleLogout} className="btn btn-primary">
+                        Logout
+                    </button>
+                </div>
+            ) : (
+                <Link to="/login" className="btn btn-primary">
+                    Login
+                </Link>
+            )}
         </div>
     );
 }
